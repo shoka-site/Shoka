64041ec8eb34302ab493289c96ae51156df52bf8
Ahmad Alshomaree <220208706@ostimteknik.edu.tr>
2026-03-16 14:50:28 +0300

    Mar 16 version 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // SEO & Performance optimizations
-    i18n: {
-        locales: ['ar', 'en'],
-        defaultLocale: 'ar',
-    },
-    
     // Ensure proper asset handling
     images: {
         remotePatterns: [
